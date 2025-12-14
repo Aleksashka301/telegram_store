6463f5ae2a0f89d58db098ab3a33c0abc6d5acfa
Aleksashka301 <aleksashka301@gmail.com>
2025-12-14 14:44:32 +0500

    Create cart_handlers.py

---
+++ handlers/cart_handlers.py
@@ -0,0 +1,93 @@
+import requests
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext
+from config import OrderStages
+
+
+def get_cart(tg_id):
+	response = requests.get('http://localhost:1337/api/carts?populate=*')
+	response.raise_for_status()
+	carts = response.json()['data']
+
+	user_cart = None
+	keyboard = []
+
+	for cart in carts:
+		if cart['tg_id'] == tg_id:
+			user_cart = cart
+			break
+
+	if user_cart and user_cart['cart_products']:
+		response = requests.get('http://localhost:1337/api/cart-products?populate=*')
+		response.raise_for_status()
+		cart_products = response.json()['data']
+
+		orders = ''
+		cost = 0
+
+		for product in cart_products:
+			if product['cart']['tg_id'] == user_cart['tg_id']:
+				orders += (
+					f'{product["product"]["title"]} \n {float(product["product"]["price"])} руб. кг. \n'
+				    f'{product["amount"]} гр {float(product["cost"])} руб. \n\n'
+				)
+				cost += product["cost"]
+
+				keyboard.append(
+					[InlineKeyboardButton(
+						f'Убрать из корзины "{product["product"]["title"]}"',
+						callback_data=f'remove_{product["documentId"]}'
+					)]
+				)
+
+		orders += f'Итого: {float(cost)} руб.'
+		keyboard.append([InlineKeyboardButton('Меню', callback_data='menu')])
+		keyboard.append([InlineKeyboardButton('Оплатить', callback_data='payment')])
+		reply_markup = InlineKeyboardMarkup(keyboard)
+		return orders, reply_markup
+
+	else:
+		keyboard.append([InlineKeyboardButton('Меню', callback_data='menu')])
+		reply_markup = InlineKeyboardMarkup(keyboard)
+		return None, reply_markup
+
+
+def show_cart(update: Update, context: CallbackContext):
+	tg_id = str(update.effective_chat.id)
+	orders, reply_markup = get_cart(tg_id)
+
+	if orders:
+		query = update.callback_query
+		query.answer()
+
+		context.bot.send_message(
+			chat_id=update.effective_chat.id,
+			text=orders,
+			reply_markup=reply_markup,
+		)
+		return OrderStages.CART
+	else:
+		context.bot.send_message(
+			chat_id=update.effective_chat.id,
+			text='Корзина пуста, вы можете добавить туда товары из меню',
+			reply_markup=reply_markup
+		)
+		return OrderStages.CART
+
+
+def remove_product_cart(update: Update, context: CallbackContext):
+	query = update.callback_query
+	query.answer()
+
+	product_document_id = query.data.split('_')[1]
+	requests.delete(f'http://localhost:1337/api/cart-products/{product_document_id}')
+
+	tg_id = str(update.effective_chat.id)
+	orders, reply_markup = get_cart(tg_id)
+
+	context.bot.send_message(
+		chat_id=update.effective_chat.id,
+		text=orders,
+		reply_markup=reply_markup,
+	)
+	return OrderStages.CART
