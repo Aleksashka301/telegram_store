cbe35d5827a50a80a35777c83900ce59d693fc35
Aleksashka301 <aleksashka301@gmail.com>
2025-12-14 14:44:44 +0500

    Create start_handlers.py

---
+++ handlers/start_handlers.py
@@ -0,0 +1,20 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext
+from config import OrderStages
+
+
+def start(update: Update, context: CallbackContext):
+	products = context.bot_data.get('products')
+	keyboard = [
+		[InlineKeyboardButton(product['title'], callback_data=f'product_{product["id"]}')]
+		for product in products
+	]
+	keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
+	reply_markup = InlineKeyboardMarkup(keyboard)
+
+	context.bot.send_message(
+		chat_id=update.effective_chat.id,
+		text='Пожалуйста, выберите товар',
+		reply_markup=reply_markup
+	)
+	return OrderStages.MENU
